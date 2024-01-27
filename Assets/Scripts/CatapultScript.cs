271b033b95b980c865b70f05b7c8a52621e4e47d
Tatus31 <137642921+Tatus31@users.noreply.github.com>
2024-01-27 12:49:29 +0100

    katapulta i poziom pathfinding

--- Assets/Scripts/CatapultScript.cs
+++ Assets/Scripts/CatapultScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CatapultScript : MonoBehaviour
 {
@@ -10,10 +11,8 @@ public class CatapultScript : MonoBehaviour
     private Transform player;
 
     private Vector3 catapultDirection;
-    private Vector3 catapultSpeedMultiplier = new Vector3(3f, 3f, 3f);
-
-    private Vector3 catapultOffset = new Vector3(5,5,5);
-
+    private Vector3 catapultSpeedMultiplier = new Vector3(25f, 25f, 25f);
+    private Vector3 catapultOffset = new Vector3(50f, 50f, 50f);
     [SerializeField]
     Camera cam;
 
@@ -25,7 +24,7 @@ public class CatapultScript : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(playerRB.transform.position.y);
+
         //if (playerRB.transform.position.y >= playerDeletePosition.y)
         //{
         //    player.transform.gameObject.SetActive(false);
@@ -35,11 +34,16 @@ public class CatapultScript : MonoBehaviour
     private void OnCatapultEnter()
     {
         catapultDirection = cam.transform.position;
-        Vector3 finalCatapultDir = new Vector3(catapultDirection.x * catapultSpeedMultiplier.x
+        Debug.Log("x" + catapultDirection.x + "y" + catapultDirection.y + "z" + catapultDirection.z);
+        Vector3 finalCatapultDir = new Vector3(catapultDirection.x + catapultOffset.x * catapultSpeedMultiplier.x
             , catapultDirection.y * catapultSpeedMultiplier.y
-            , catapultDirection.z * catapultSpeedMultiplier.z);
+            , -catapultDirection.z + -catapultOffset.z* catapultSpeedMultiplier.z);
 
-        playerRB.AddForce(finalCatapultDir, ForceMode.Impulse);
+        playerRB.AddForce(finalCatapultDir, ForceMode.Acceleration);
+        //for (int i = 0; i <= 2; i++)
+        //{
+        //    catapultDirection[i] += Mathf.Lerp(playerRB.transform.position[i], catapultDirection[i], catapultSpeedMultiplier[i]);
+        //}
     }
 
     private void OnCollisionEnter(Collision collision)
