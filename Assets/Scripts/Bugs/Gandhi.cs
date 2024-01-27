5f4f8444b9b580e9f4cdb6dc7cf931e00c59a8c5
SiogunJH <homarofficial@gmail.com>
2024-01-27 16:45:56 +0100

    Ghandi

--- Assets/Scripts/Bugs/Gandhi.cs
+++ Assets/Scripts/Bugs/Gandhi.cs
@@ -6,11 +6,11 @@ public class Gandhi : BugBase
 {
     void OnCollisionEnter(Collision collision)
     {
-        // Check if the collision is with a GameObject tagged as "Player"
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Perform actions specific to collision with the player
-            Debug.Log("Collision with the player!");
+            Invoke("GameOverReference", 3);
+            GetComponent<AudioSource>().Play();
+
         }
     }
 }
