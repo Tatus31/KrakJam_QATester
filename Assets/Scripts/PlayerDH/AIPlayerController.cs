5429a9e177d3ab5cf8d5db8e5d1e44c47713a9cc
Danylo Hukov <48653308+DHukov@users.noreply.github.com>
2024-01-27 11:17:41 +0100

    TargetManager

--- Assets/Scripts/PlayerDH/AIPlayerController.cs
+++ Assets/Scripts/PlayerDH/AIPlayerController.cs
@@ -1,10 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using System.Linq;
-
-
 
 public class AIPlayerController : MonoBehaviour
 {
@@ -12,29 +8,17 @@ public class AIPlayerController : MonoBehaviour
     private NavMeshAgent meshAgent;
     [SerializeField] private Transform currentTarget;
 
-
-    private void OnEnable()
-    {
-
-    }
-    private void OnDisable()
-    {
-
-    }
     void Start()
     {
         targetManager = GetComponent<PathfinderTargetManager>();
         meshAgent = GetComponent<NavMeshAgent>();
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        currentTarget = targetManager.UpdateTarget();
+        currentTarget = targetManager.SetNearestTarget();
         meshAgent.SetDestination(currentTarget.position);
 
 
     }
-
-   
 }
