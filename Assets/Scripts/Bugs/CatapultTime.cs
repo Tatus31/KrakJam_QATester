7b437f6c8ef9a5007b8f8dfd4e803d0bc7bc1027
Tatus31 <137642921+Tatus31@users.noreply.github.com>
2024-01-28 13:26:18 +0100

    Katapulta

--- Assets/Scripts/Bugs/CatapultTime.cs
+++ Assets/Scripts/Bugs/CatapultTime.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class CatapultTime : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>();
+        StartCatapultTimer();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void StartCatapultTimer()
+    {
+        StartCoroutine(CatapultTimer());
+    }
+
+    private IEnumerator CatapultTimer()
+    {
+        yield return new WaitForSeconds(1f);
+        rb.AddForce(Vector3.up * 50f, ForceMode.Impulse);
     }
 }
